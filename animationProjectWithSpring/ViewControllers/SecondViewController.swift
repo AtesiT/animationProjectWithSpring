00bb38f8ed5d1589fa1e5204e9140c6539539f8a
AtesiT <atesit@mail.ru>
2025-11-29 14:57:47 +0300

    bug: almost was added animation and displays of numbers on a view (but needs a fix for it)

--- animationProjectWithSpring/ViewControllers/SecondViewController.swift
+++ animationProjectWithSpring/ViewControllers/SecondViewController.swift
@@ -3,15 +3,32 @@ import SpringAnimation
 
 class SecondViewController: UIViewController {
 
+    var animation: Animation!
+    
     @IBOutlet var animationView: SpringView!
     @IBOutlet var labelsOfView: [UILabel]!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
     @IBAction func animationButton(_ sender: SpringButton) {
+        print(generateRandomAnimation())
+        labelsOfView[0].text = "\(animation.current)"
+        labelsOfView[1].text = "\(animation.curve)"
+        labelsOfView[2].text = String(format: "%.1f", animation.force)
+        labelsOfView[3].text = String(format: "%.1f", animation.duration)
+        labelsOfView[4].text = String(format: "%.1f", animation.delay)
+        
+        animationView.animation = String(animation.current)
+        animationView.curve = String(animation.curve)
+        animationView.force = CGFloat(animation.force)
+        animationView.duration = CGFloat(animation.duration)
+        animationView.delay = CGFloat(animation.delay)
+        animationView.animate()
+        
     }
+    func generateRandomAnimation() -> [Any] {
+        animation = Animation()
+        return animation.randomAnimation()
+    }
+    
     @IBAction func backButton(_ sender: SpringButton) {
         dismiss(animated: true)
     }
